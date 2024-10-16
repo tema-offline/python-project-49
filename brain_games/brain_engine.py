ab311d5c7f2534dd52b0c3bdc801aee3cb14d86d
tema-offline <tema.offline@gmail.com>
2024-10-16 16:29:31 +0300

    engine

--- brain_games/brain_engine.py
+++ brain_games/brain_engine.py
@@ -1,9 +1,10 @@
-from brain_games.cli import welcome_user
 import prompt
 
 
 def game_run(game):
-    player_name = welcome_user()
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
     print(game.RULES)
     correct_answer = 0
     for _ in range(3):
@@ -13,10 +14,10 @@ def game_run(game):
         if player_answer != str(answer):
             print(f"'{player_answer}' is wrong answer ;(. "
                   f"Correct answer was '{answer}'.\n"
-                  f"Let's try again, {player_name}!")
+                  f"Let's try again, {name}!")
             break
         else:
             print('Correct!')
         correct_answer += 1
     if correct_answer == 3:
-        print(f'Congratulations, {player_name}!')
+        print(f'Congratulations, {name}!')
